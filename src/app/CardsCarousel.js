273cdb122c3e3a872860953cfddc30453837ad96
khaine <phankhai1794@gmail.com>
2023-08-19 22:34:58 +0700

    update template

--- src/app/CardsCarousel.js
+++ src/app/CardsCarousel.js
@@ -11,6 +11,42 @@ const CardsCarousel = (props) => {
         rows: 1,
         slidesToShow: 6,
         slidesToScroll: 6,
+        initialSlide: 0,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     };
 
     return (
