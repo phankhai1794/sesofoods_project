36744acd8447e1a777df60861428815048fe78d2
khaine <phankhai1794@gmail.com>
2023-08-19 08:16:57 +0700

    project demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Home from "./app/Home";
+import {NavbarComponent, OffcanvasExample} from "./app/NavbarComponent";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+        <Home />
+        <NavbarComponent />
+        <OffcanvasExample />
     </div>
   );
 }
---
+++ src/app/NavbarComponent.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import {Container, Nav, Navbar, NavDropdown, Button, Form, Offcanvas} from "react-bootstrap";
+
+export const NavbarComponent = () => {
+    return (
+        <div>
+            <Navbar expand="lg" className="bg-body-tertiary">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export const OffcanvasExample = () => {
+    return (
+        <>
+            {['xl'].map((expand) => (
+                <Navbar key={expand} expand={expand} className="bg-body-tertiary mb-3">
+                    <Container fluid>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+
+                        {/*Logo*/}
+                        <Navbar.Brand href="#">Logo</Navbar.Brand>
+
+                        {/*Search and Menu*/}
+                        <Navbar.Brand href="#">Menu and Search</Navbar.Brand>
+
+                        {/*Toggle navbar responsive*/}
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="start"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Offcanvas
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="#action1">Home</Nav.Link>
+                                    <Nav.Link href="#action2">Link</Nav.Link>
+                                    <NavDropdown
+                                        title="Dropdown"
+                                        id={`offcanvasNavbarDropdown-expand-${expand}`}
+                                    >
+                                        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action4">
+                                            Another action
+                                        </NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="#action5">
+                                            Something else here
+                                        </NavDropdown.Item>
+                                    </NavDropdown>
+                                </Nav>
+                                <Form className="d-flex">
+                                    <Form.Control
+                                        type="search"
+                                        placeholder="Search"
+                                        className="me-2"
+                                        aria-label="Search"
+                                    />
+                                    <Button variant="outline-success">Search</Button>
+                                </Form>
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+
+                    </Container>
+                </Navbar>
+            ))}
+        </>
+    )
+}
