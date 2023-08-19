5b0a88e573be1b7c2f3a45bd738db842e9d4dc99
khaine <phankhai1794@gmail.com>
2023-08-19 07:08:34 +0700

    init project demo

---
+++ src/app/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Button, Stack} from "react-bootstrap";
+
+const Home = () => {
+    return (
+        <Stack direction="horizontal" gap={2}>
+            <Button as="a" variant="primary">
+                Button as link
+            </Button>
+            <Button as="a" variant="success">
+                Button as link
+            </Button>
+        </Stack>
+    );
+};
+
+export default Home;
